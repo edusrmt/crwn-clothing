378627fa9fb88998ac77ad1aeca63702fcfbaa3a
Eduardo Sarmento <cealvesarmento@gmail.com>
2023-02-28 13:26:19 -0300

    Implement basic routing

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <>
+      <div className='navigation'>
+        <div className='logo-container'>
+          <Link className='logo-container' to='/'>
+            <CrwnLogo className='logo' />
+          </Link>
+        </div>
+
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+}
+
+export default Navigation;
